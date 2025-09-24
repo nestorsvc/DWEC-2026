49675e1bfba72da2387d24e4475167716ce4a3b1
nestorsvc <nestorserna100@gmail.com>
2025-09-24 19:14:55 +0200

    seguimos 5

--- Logica/Ejercicio05/app.js
+++ Logica/Ejercicio05/app.js
@@ -3,11 +3,22 @@ function rombo(){
     let n = 5;
     for (let i = 0; i < n; i++ ){
         rombo[i] = '';
-        for(let j = 0; j < n - 1 -i; j++){
+        for(let j = 0; j < n - 1 - i; j++){
             rombo[i] += ' ';
         }
-        for (let x = 0; x < 2 * i - 1; x++){
+        for (let x = 0; x < 2 * i + 1; x++){
             rombo[i] += '*';
         }
     }
+    for (let i = n - 2; i > 0; i--){
+        for(let j = 0; j < n - 1 - i; j++){
+            rombo[i] += ' ';
+        }
+        for (let x = 0; x < 2 * i + 1; x++){
+            rombo[i] += '*';
+        }
+    }
+    console.log(rombo);
+    
 }
+rombo();
