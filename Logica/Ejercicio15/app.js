79df49aa0f6084bcc992457c8a9ad915e0ce1ecd
nestorsvc <nestorserna100@gmail.com>
2025-09-30 20:12:47 +0200

    15 casi hecho

--- Logica/Ejercicio15/app.js
+++ Logica/Ejercicio15/app.js
@@ -12,6 +12,7 @@ function listarTrabajadores(trabajadores) {
     });
     return alert(resultado);
 }
+
 function crearTrabajador() {
     let nombre = prompt('Introduce el nombre del nuevo trabajador');
     let categoria = parseInt(prompt('Introduce la categoría del nuevo trabajador'));
@@ -26,22 +27,97 @@ function crearTrabajador() {
     // }
 
     let nuevoTrabajador = { codigo: codigo, nombre: nombre, categoria: categoria, contratacion: contratacion };
-    //* push muta el array, concat devuelve uno nuevo
 
+    //* push muta el array, concat devuelve uno nuevo
     trabajadores = trabajadores.concat(nuevoTrabajador);
-    return trabajadores;
+    return listarTrabajadores(trabajadores);
 }
 
 function borrarTrabajador(codigo) {
     let confirmacion = confirm('¿Está seguro de que quiere borrar al trabajador?');
     if (confirmacion) {
-        let trabajadoresEncontrados = trabajadores.filter(e => e.codigo !== codigo);
-        return trabajadoresEncontrados;
+        let trabajadoresEncontrados = trabajadores.filter(t => t.codigo !== codigo);
+        return listarTrabajadores(trabajadoresEncontrados);
     } else{
         alert('Operación cancelada');
     }
-    return trabajadores;
+    return listarTrabajadores(trabajadores);
 }
 
-let probar = borrarTrabajador('E02');
-console.log(probar);
+
+function modificarTrabajador(codigo){
+    let trabajadorEncontrado = trabajadores.filter(t => t.codigo == codigo); 
+    let valoresActuales = '';
+    trabajadorEncontrado.forEach(trabajador => {
+         valoresActuales+= `Nombre: ${trabajador.nombre}\nCategoría: ${trabajador.categoria}\nContratación: ${trabajador.contratacion}`;
+    });
+    
+    let confirmacion = confirm(`¿Está seguro que quiere modificar el trabajador seleccionado?\nValores por defecto:\n${valoresActuales}`);
+    if(confirmacion){
+        let nuevoNombre = prompt('Introduce el nombre para modificar el actual');
+        let nuevaCategoria = prompt('Introduce la categoria para modificar la actual');
+        let nuevaContratacion = prompt('Introduce la contratacion para modificar la actual');
+        if(nuevoNombre!=null && nuevaCategoria!=null && nuevaContratacion!=null){
+            for (i of trabajadorEncontrado){
+                i.nombre = nuevoNombre;
+                i.categoria = nuevaCategoria;
+                i.contratacion = nuevaContratacion;
+            }
+            return listarTrabajadores(trabajadorEncontrado);
+        }else {
+            alert('Debe rellenar todos los campos');
+        }
+    }else{
+        alert('El trabajador no se ha modificado');
+        return listarTrabajadores(trabajadorEncontrado);
+    }
+}
+
+function listarNominas(){
+    let listadoNominas = '--LISTADO DE NOMINAS--\n';
+    let importeTotal = 0;
+    for (i of trabajadores){
+        listadoNominas += `Trabajador: ${i.nombre} | Categoría: ${i.categoria} | Nomina Final =  ${calcularBaseAntiguedad(i)}€\n`;
+        importeTotal += calcularBaseAntiguedad(i);
+    }
+    listadoNominas+=`Gasto total en nominas: ${importeTotal}€`;
+    
+    return alert(listadoNominas);
+}
+function calcularBaseAntiguedad(trabajador){
+    let objetoAnio = new Date();
+    let anio = objetoAnio.getFullYear();
+    let aniosTrabajados = anio - trabajador.contratacion;
+    let nominaTrabajador = 0;
+    let antiguedad = 0.04;
+
+    switch(trabajador.categoria){
+        case 1: 
+        nominaTrabajador = 1100 + (1100 * aniosTrabajados * antiguedad);
+        break;
+        case 2:
+        nominaTrabajador = 1400 + (1400 * aniosTrabajados * antiguedad);
+        break;
+        case 3:
+        nominaTrabajador = 1900 + (1900 * aniosTrabajados * antiguedad);
+        break;
+    }
+    return nominaTrabajador;
+}
+
+function iniciarApp(){
+
+    let opcion = prompt('Menú gestión de empresa: Trabajadores\nSelecciona una opción:\n1.Listar Trabajadores\n2.Crear Trabajador\n3.Borrar Trabajador\n4.Modificar Trabajador\n5.Listar Nóminas');
+    
+    
+
+
+}
+let probarListarTrabajadores = listarTrabajadores(trabajadores);
+let probarCrearTrabajador = crearTrabajador();
+let probarBorrarTrabajador = borrarTrabajador("E01");
+let probarModificarTrabajador = modificarTrabajador("E03");
+let probarListarNominas = listarNominas();
+console.log(probarListarNominas);
+
+
