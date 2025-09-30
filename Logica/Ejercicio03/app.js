3addf8d40e1c84ed22ed2fc8ef8dc840469eec89
proscojonzio <nestorserna100@gmail.com>
2025-09-30 16:34:20 +0200

    rectangulo hecho como tiene que ser

--- Logica/Ejercicio03/app.js
+++ Logica/Ejercicio03/app.js
@@ -1,21 +1,38 @@
 'use strict';
 
-function cuadradoRectangulo(){
-    let n = parseInt(prompt('Introduce la altura del rectangulo:'));
-    let cuadrado = [];
-    for (let i = 0; i <= n-1; i++){
-        cuadrado[i] = '';                
-        for (let j = 0; j <= n - 1; j++){
-            if(i == 0|| i == n-1 || j == 0 || j == n-1){
-                cuadrado[i] += '*';
-            }else{
-                cuadrado[i] += ' ';
+// function cuadradoRectangulo(){
+//     let n = parseInt(prompt('Introduce la altura del rectangulo:'));
+//     let cuadrado = [];
+//     for (let i = 0; i <= n-1; i++){
+//         cuadrado[i] = '';                
+//         for (let j = 0; j <= n - 1; j++){
+//             if(i == 0|| i == n-1 || j == 0 || j == n-1){
+//                 cuadrado[i] += '*';
+//             }else{
+//                 cuadrado[i] += ' ';
+//             }
+//         }
+//     }
+//     return cuadrado;
+// }
+
+
+
+function dibujarRectangulo(base, altura){
+    let resultado = '';
+
+    for (let i = 0; i <= altura - 1; i++){
+        for (let j = 0; j <= base - 1; j++){
+            if (j == 0 || i == 0 || j == base - 1 || i == altura - 1){
+                resultado+= '*';
+            } else {
+                resultado+= ' ';
             }
         }
+        resultado+='\n';
     }
-    return cuadrado;
+    return resultado;
 }
-
-let resultado = cuadradoRectangulo();
+let resultado = dibujarRectangulo(5,8);
 console.log(resultado);
 
