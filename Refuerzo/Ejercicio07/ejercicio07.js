b4f1318b09d2405a336536c4eb73be6cc3476f1d
nestorsvc <nestorserna100@gmail.com>
2025-10-08 20:09:57 +0200

    7

--- Refuerzo/Ejercicio07/ejercicio07.js
+++ Refuerzo/Ejercicio07/ejercicio07.js
@@ -1,20 +1,32 @@
-console.log('holsada');
-
 function conjuntos (arr, arr1, operacion){
-    console.log('gajsoipfjsaojidoi');
-    
+
     let arrInterseccion = [];
     let arrUnion = [];
     let arrDiferencia = [];
     let operacionMinuscula = operacion.toLowerCase();
     if (operacionMinuscula == 'union'){
-       for (let i = 0; i < arr.length ; i++){
+        let contadorApariciones = {};
         
-        if(arr[i] !== arr1[i]){
-            arrUnion[i] = i;
+       for (let i = 0; i < arr.length ; i++){
+        if(contadorApariciones[arr[i]] == undefined){
+            contadorApariciones[arr[i]] = 1;
+        } else {
+            contadorApariciones[arr[i]] -= 1;
         }
        }
+
+       for(i in contadorApariciones){
+           if (contadorApariciones[i] !== 0){
+               for (let x = 0; x <= arrUnion.length; x++){
+                   arrUnion[x] = i;
+            }
+        }
+       }
+      console.log(arrUnion);
+      
     }
+    
+    
 
     if(operacionMinuscula == 'interseccion'){
 
@@ -23,9 +35,9 @@ function conjuntos (arr, arr1, operacion){
     if(operacionMinuscula == 'diferencia'){
 
     }
-    return arrUnion;
+    // return arrUnion;
 }
-let arr = [1,2,3]
+let arr = [1,2,3,3]
 let arr1 = [2,3,4]
 let resultado = conjuntos(arr,arr1,'union');
 console.log(resultado);
